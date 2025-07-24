bb6b80c666d2395dd99f054b042db29129265da6
Maria Ilina <m.polibit@gmail.com>
2025-07-24 23:33:31 +0300

    refactor: spiner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,13 +1,9 @@
-import { Component } from 'react';
-
 import styles from './Spinner.module.scss';
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <div className={styles.spinnerWrapper} role="status" aria-label="loading">
-        <div className={styles.loading}>{`<Loading `}</div>
-      </div>
-    );
-  }
+export function Spinner() {
+  return (
+    <div className={styles.spinnerWrapper} role="status" aria-label="loading">
+      <div className={styles.loading}>{`<Loading `}</div>
+    </div>
+  );
 }
