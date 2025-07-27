9f409adcdb436b9677600f53ebbcae6b97f091a8
Maria Ilina <m.polibit@gmail.com>
2025-07-28 00:27:51 +0300

    feat: add about and not-found pages

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,3 +1,20 @@
+import styles from './AboutPage.module.scss';
+
 export function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <section>
+      <div className={styles.wrapper}>
+        <h1>About</h1>
+        <span>What I can tell about myself:</span>
+        <p>
+          {`I'm trying to learn, but let's be honest — I'm more of a “try” than a “succeed” kind of
+          guy. Proud student at RS School, mostly making sad noises and occasionally creating
+          something useful... or at least entertaining.`}
+        </p>
+        <a className={styles.link} href="https://rs.school" target="blank">
+          <img className={styles.rsLogo} src="./rss-logo.png" alt="rss-logo" />
+        </a>
+      </div>
+    </section>
+  );
 }
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,16 @@
+import { useNavigate } from 'react-router';
+
+import styles from './NotFoundPage.module.scss';
+
 export function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section className={styles.wrapper}>
+      <h1>Page Not Found</h1>
+      <div>The page you&apos;re looking for doesn&apos;t exist or has been moved.</div>
+
+      <button onClick={() => navigate(`/`)}>Back to the main page</button>
+    </section>
+  );
 }
