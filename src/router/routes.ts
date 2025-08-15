c1ec848eb8d6996ec78ec50661e6034de20999de
Maria Ilina <m.polibit@gmail.com>
2025-08-16 00:34:53 +0300

    refactor: remove tests and other useless code

--- src/router/routes.ts
+++
@@ -1,5 +0,0 @@
-export const ROUTES = {
-  ABOUT: 'about',
-  NOT_FOUND: 'not-found',
-  HOME: '/',
-};
--- src/test-utils/randomizers.ts
+++
@@ -1,6 +0,0 @@
-export const getRandomNumber = (min: number = 0, max: number = 10) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-
-export const getRandomItemFromArray = <T>(array: T[]) => {
-  return array[getRandomNumber(0, array.length - 1)];
-};
