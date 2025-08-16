cd4ac0c5e4c30634e179bf9ab4ded7a373ce948e
Maria Ilina <m.polibit@gmail.com>
2025-08-16 22:56:12 +0300

    chore: update environment

---
+++ eslint.config.ts
@@ -0,0 +1,52 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default tseslint.config([
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  eslintPluginPrettier,
+
+  { ignores: ['dist'] },
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'react/jsx-no-useless-fragment': 'error',
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+      ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      curly: 'error',
+    },
+  },
+]);
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from 'next';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   distDir: './dist',
 
   webpack(config) {
